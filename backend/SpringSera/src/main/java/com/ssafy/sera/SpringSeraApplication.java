30263aaa7a9bc6ef5b23ab45e51937507b34e2dc
hyeji6138 <hyeji6138@naver.com>
2021-03-11 10:07:57 +0900

    backend project file upload

---
+++ backend/SpringSera/src/main/java/com/ssafy/sera/SpringSeraApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.sera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSeraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSeraApplication.class, args);
+	}
+
+}

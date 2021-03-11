ca01307fdc35c07001142263cff7cc04d176bcbe
Ji-hyeong <qmffn38@gmail.com>
2021-03-12 00:32:59 +0900

    FE_0312: React project setup

--- frontend/src/app.jsx
+++ frontend/src/app.jsx
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
 import './app.css';
-
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Home from './router/Home/Home';
+import Login from './router/Login/Login';
+import SignUp from './router/SignUp/SignUp';
+import FindPW from './router/FindPW/FindPW';
+import MyPage from './router/MyPage/MyPage';
+import MyPick from './router/MyPick/MyPick';
+import CosmeticList from './router/CosmeticList/CosmeticList';
+import CosmeticDetail from './router/CosmeticDetail/CosmeticDetail';
+import SkinType from './router/SkinType/SkinType';
+import PersonalColor from './router/PersonalColor/PersonalColor';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Switch>
+          <Route>
+            <Home exact path="/"/>
+          </Route>
+          <Route>
+            <Login path="/login"/>
+          </Route>
+          <Route>
+            <SignUp path="/signup"/>
+          </Route>
+          <Route>
+            <FindPW path="/findpw"/>
+          </Route>
+          <Route>
+            <MyPage path="/mypage"/>
+          </Route>
+          <Route>
+            <MyPick path="/mypick"/>
+          </Route>
+          <Route>
+            <CosmeticList path="/list"/>
+          </Route>
+          <Route>
+            <CosmeticDetail path="/detail/:id"/>
+          </Route>
+          <Route>
+            <SkinType path="/skintype"/>
+          </Route>
+          <Route>
+            <PersonalColor path="/personal_color"/>
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }

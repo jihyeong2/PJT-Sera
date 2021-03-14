44a3a0b9202fe2fb2398dd64f0a6aea23b4f92c9
유진이 <wlsdk400@naver.com>
2021-03-14 20:19:28 +0900

    FE_0314:상품디테일 진행중

---
+++ frontend/src/components/CosmeticDetail/detail_left.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import styles from './detail.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+const Detail = ( ) => (
+    <div className={styles.detail_left}>
+        <img className={styles.product_image} src={process.env.PUBLIC_URL + '/images/product_Sample.PNG'} alt="상품사진"/>
+        <p className={styles.match}><span className={styles.match_name} >일치율 </span><span className={styles.match_percent}>80%</span></p>
+        <div className={styles.icon}>
+            <span className={styles.heart_icon}><FontAwesomeIcon  icon={['far', 'heart']} size="2x" color="red" /></span>
+            {/* <FontAwesomeIcon icon={['fas', 'heart']} size="2x" color="red"/> */}
+            <span className={styles.share_icon}><FontAwesomeIcon icon="share-alt" size="lg"  color="gray"/></span>
+        </div>
+    </div>           
+);
+
+export default Detail;

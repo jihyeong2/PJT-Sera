97cd5723331e67fc81444ca904b8257fa3182cdf
Ji-hyeong <qmffn38@gmail.com>
2021-03-15 23:29:12 +0900

    FE_0315 : common 탑 버튼 생성

--- frontend/src/FontAwesome.jsx
+++ frontend/src/FontAwesome.jsx
@@ -1,5 +1,5 @@
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faHeart as fasHeart, faShareAlt} from '@fortawesome/free-solid-svg-icons';
+import { faChevronRight, faChevronUp, faHeart as fasHeart, faShareAlt} from '@fortawesome/free-solid-svg-icons';
 import { faHeart as farHeart} from '@fortawesome/free-regular-svg-icons'
 
-library.add(fasHeart, farHeart, faShareAlt);
+library.add(fasHeart, farHeart, faShareAlt,faChevronUp,faChevronRight);

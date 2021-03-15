df451276fc9f5894815ff7afa931848ac9351b77
Ji-hyeong <qmffn38@gmail.com>
2021-03-16 00:11:50 +0900

    FE_0316 : common 로고 생성

--- frontend/src/router/MyPage/MyPage.jsx
+++ frontend/src/router/MyPage/MyPage.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import styles from './Mypage.module.css';
 import SkinInfo from '../../components/MyPage/SkinInfo/SkinInfo';
 import MyPageTabs from '../../components/MyPage/MyPageTabs/MyPageTabs';
+import Logo from '../../components/common/Logo/Logo';
 
 const MyPage = (props) => {
   return (
     <div className={styles.container}>
       {/* <Navbar/> */}
-      {/* <Header/> */}
+      <Logo type={1}/>
       <header className={styles.title}>
         마이페이지
       </header>

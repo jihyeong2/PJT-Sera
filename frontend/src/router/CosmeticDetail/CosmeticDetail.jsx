6bd4b94d3a5fa459ac0b178765b9628093da162d
유진이 <wlsdk400@naver.com>
2021-03-15 14:36:47 +0900

    FE_0315:상세페이지 제작중

--- frontend/src/router/CosmeticDetail/CosmeticDetail.jsx
+++ frontend/src/router/CosmeticDetail/CosmeticDetail.jsx
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import DetailLeft from '../../components/CosmeticDetail/detail_left';
 import DetailRight from '../../components/CosmeticDetail/detail_right';
-import Youtube from '../../components/CosmeticDetail/youtube';
+import Youtube from '../../components/CosmeticDetail/youtube_list';
 import Review from '../../components/CosmeticDetail/review';
 import styles from './CosmeticDetail.module.css'
 import Grid from '@material-ui/core/Grid';
 
 const CosmeticDetail = ( ) => {
+    const [videos, setVideos] = useState([]);
+
+    useEffect(() => {
+        const requestOptions = {
+            method: 'GET',
+            redirect: 'follow'
+          };
+          
+          fetch("https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=5&key=AIzaSyAIAN4fWbhQxYcuLU-cnjAGihX695m5azE", requestOptions)
+            .then(response => response.json())
+            .then(result => setVideos(result.items))
+            .catch(error => console.log('error', error));
+    }, []); // 마운트가 되었을 때만 호출
+
     return (
         <div className={styles.page}>
             <Grid container spacing={4}>
@@ -17,8 +31,9 @@ const CosmeticDetail = ( ) => {
                 <DetailRight />
             </Grid>
             <Grid item xs={12} className={styles.youtube}>
-                <Youtube />
+                <Youtube videos={videos} />
             </Grid>
+            <div className={styles.bar}></div>
             <Grid item xs={12} className={styles.review}>
                 <Review />
             </Grid>

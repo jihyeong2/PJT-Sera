356b1e523debc732cdea9f1fbace244d85308399
유진이 <wlsdk400@naver.com>
2021-03-17 06:37:19 +0900

    FE_0317:common에 안쓰는거 주석함

--- frontend/src/components/common/SearchBar/SearchBar.jsx
+++ frontend/src/components/common/SearchBar/SearchBar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import CloseIcon from '@material-ui/icons/Close';
+// import CloseIcon from '@material-ui/icons/Close';
 import SearchIcon from '@material-ui/icons/Search';
 import IconButton from '@material-ui/core/IconButton';
 import styles from './SearchBar.module.css';

8eaead6157e39ff75b05fd4ab8fe2092a893e11b
유진이 <wlsdk400@naver.com>
2021-03-18 10:25:26 +0900

    FE_0318:상품디테일 거의 완성

--- frontend/src/components/CosmeticDetail/review_head.jsx
+++ frontend/src/components/CosmeticDetail/review_head.jsx
@@ -1,12 +1,32 @@
 import React from 'react';
 import styles from './review.module.css';
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
+import Rating from '@material-ui/lab/Rating';
+import Box from '@material-ui/core/Box';
 
-const Review = (props) => {
+const ReviewHead = (props) => {
+    // const [value, setValue] = React.useState(2);
     return(
         <div className={styles.review_head} >
-            <h1>리뷰 헤드</h1>
+            <Grid container spacing={4}>
+                <Grid item xs={4}>
+                    <p className={styles.review_num_title}>총 <span>23</span>건의 리뷰</p>
+                    <p className={styles.review_rate}><span>4.7</span>점</p>
+                    <Box component="fieldset" mb={3} borderColor="transparent">
+                        <Rating name="read-only" value={4} readOnly />
+                    </Box>
+                </Grid>
+                <Grid item xs={4}>
+                    차트
+                </Grid>
+                <Grid item xs={4}>
+                    <p className={styles.review_btn_title}>리뷰를 써보세요.</p>
+                    <Button className={styles.review_btn} variant="contained">리뷰작성</Button>
+                </Grid>
+            </Grid>
         </div>
     );
 }
 
-export default Review;
+export default ReviewHead;

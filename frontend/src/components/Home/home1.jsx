85e172d0f74cb54ba3bc3833d5d743a290a4c689
Ji-hyeong <qmffn38@gmail.com>
2021-03-23 03:24:44 +0900

    FE_0323 : 메인화면 풀페이지 스크롤 구현

---
+++ frontend/src/components/Home/home1.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Home1 = (props) => {
+  return (
+    <h1>Home1</h1>
+  );
+}
+
+export default Home1;
---
+++ frontend/src/components/Home/home3.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Home3 = (props) => {
+  return (
+    <div>Home3</div>
+  )
+};
+
+export default Home3;

6c749935fc77e6bafcf07d6650216eee279cee2b
유진이 <wlsdk400@naver.com>
2021-03-25 04:37:02 +0900

    FE_0325:디테일페이지 바뀐부분 UI 수정, 유튜브리스트 완료, 유튜브 모달제작완료, 최저가 이동완료

--- frontend/src/components/CosmeticDetail/detail/detail_left.jsx
+++ frontend/src/components/CosmeticDetail/detail/detail_left.jsx
@@ -1,30 +1,6 @@
 import React from 'react';
 import styles from './detail.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-// import ReactWordcloud from "react-wordcloud";
-
-// import "tippy.js/dist/tippy.css";
-// import "tippy.js/animations/scale.css";
-
-// const words = [
-//     {
-//       text: 'told',
-//       value: 64,
-//     },
-//     {
-//       text: 'mistake',
-//       value: 11,
-//     },
-//     {
-//       text: 'thought',
-//       value: 16,
-//     },
-//     {
-//       text: 'bad',
-//       value: 17,
-//     },
-//   ]
-
 
 const Detail = ( ) => (
     <div className={styles.detail_left}>
@@ -37,9 +13,6 @@ const Detail = ( ) => (
             231<span className={styles.heart_icon}><FontAwesomeIcon  icon={['far', 'heart']} size="lg" color="red" /></span>
             {/* <FontAwesomeIcon icon={['fas', 'heart']} size="2x" color="red"/> */}
             <div className={styles.share_icon}><FontAwesomeIcon icon="share-alt" size="lg"  color="gray"/></div>
-        </div>
-        <div className={styles.wordcloud}>
-            {/* <ReactWordcloud words={words} /> */}
         </div>
     </div>           
 );

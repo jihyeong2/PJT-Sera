2ca12655cc70fe9370b11124ee58dd1f45d430bd
Ji-hyeong <qmffn38@gmail.com>
2021-03-29 10:05:53 +0900

    FE_0329 : 메인 중간

--- frontend/src/components/Home/home1.jsx
+++ frontend/src/components/Home/home1.jsx
@@ -1,16 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './home1.scss';
+import Logo from '../common/Logo/Logo';
 import mainImg from '../../assets/mainz.png';
 const Home1 = (props) => {
+  const [typeNum,setTypeNum] = useState(1);
+  // console.log(props);
+  const onClick = (e) =>{
+    if(e.target.checked){
+      setTypeNum(3);
+      props.onClickImage();
+    } else{
+      setTypeNum(1);
+    }
+  }
   return (
       <div class="wrapper">
-        <input type="checkbox"/>
+        <Logo type={typeNum} style={{float:'left',}}/>
+        <input type="checkbox" onClick={onClick}/>
         <div class="video">
           <img src={mainImg} alt="사진"/>
         </div>
-        <div class="text">
-          <span data-text="Click"></span>
-        </div>
+        {/* <div class="text">
+          <span data-text="Click Me fdsajklfdjskalfjdslkk"></span>
+        </div> */}
       </div>
   );
 }
--- frontend/src/router/Home/Home.jsx
+++ frontend/src/router/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './test.css';
 import Home1 from '../../components/Home/home1';
 import Home2 from '../../components/Home/home2';
@@ -6,6 +6,9 @@ import Home3 from '../../components/Home/home3';
 import Home4 from '../../components/Home/home4';
 import Navbar from '../../components/common/Navbar/Navbar';
 const Home = (props) => {
+  const [isWhite, setIsWhite] = useState(false);
+  const [isClicked, setIsClicked] = useState(false);
+  console.log(isClicked,isWhite);
   useEffect(()=>{
     const sections = document.querySelectorAll('section');
     const content = document.querySelector('.main__content');
@@ -48,37 +51,51 @@ const Home = (props) => {
       // content.style.transform = 'translateY(-'+ count*100 +'vh)';
       document.querySelector('.sec_button.active').classList.remove('active');
       buttons[count].classList.add('active');
+      if(isClicked){
+        if(count===0) setIsWhite(true);
+        else setIsWhite(false);
+      } else{
+        setIsWhite(false);
+      }
     }
     return () => {
       window.removeEventListener('mousewheel',wheelEvent);
     }
-  },[])
+  },[isClicked]);
+  const onClickImage = () => {
+    console.log(document.querySelector('.section_navigation').style);
+    document.querySelector('.section_navigation').style.cssText='opacity: 1;';
+    setIsClicked(true);
+    setIsWhite(true);
+  };
   return(
     <div className="fullscreen">
-      <Navbar/>
+      <div style={{width:'80%'}}>
+        <Navbar white={isWhite}/>
+      </div>
       <div className="main__content">
-        <section data-title="Home" className="fbx" style={{backgroundColor:'#000000',}}>
+        <section data-title="Home" className="fbx" style={{backgroundColor:'#FFFFFF',}}>
           <div className="sec_container">
-            <Home1/>
+            <Home1 onClickImage={onClickImage}/>
           </div>
         </section>
-        <section data-title="Services" className="fbx" style={{backgroundColor:'#000000'}}>
+        <section data-title="Services" className="fbx" style={{backgroundColor:'#FFFFFF'}}>
           <div className="sec_container">
             <Home2/>
           </div>
         </section>
-        <section data-title="Contact us" className="fbx" style={{backgroundColor:'#000000'}}>
+        <section data-title="Contact us" className="fbx" style={{backgroundColor:'#FFFFFF'}}>
           <div className="sec_container">
             <Home3/>
           </div>
         </section>
-        <section data-title="Contact us" className="fbx" style={{backgroundColor:'#000000'}}>
+        <section data-title="Contact us" className="fbx" style={{backgroundColor:'#FFFFFF'}}>
           <div className="sec_container">
             <Home4/>
           </div>
         </section>
       </div>
-      <div className="section_navigation">
+      <div className="section_navigation" style={{opacity:0,}}>
         <div className="sec_button"></div>
         <div className="sec_button"></div>
         <div className="sec_button"></div>

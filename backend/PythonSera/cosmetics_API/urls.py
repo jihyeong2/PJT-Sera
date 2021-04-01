623a71c647b16a6748b76af1ee988a736856c08b
hyeji6138 <hyeji6138@naver.com>
2021-04-01 14:21:44 +0900

    BE_0401 : 퍼스널컬러 api 작업중
    Former-commit-id: 8e38122fcff0ec5830999f2ce01696703682bd10 [formerly 9be1819600877c830ef235ff92bcd76230a1cc76]
    Former-commit-id: 44f8a9bd1f020fff28bcbb75f89525dbc3f2a4c2

--- backend/PythonSera/cosmetics_API/urls.py
+++ backend/PythonSera/cosmetics_API/urls.py
@@ -2,19 +2,19 @@ from django.urls import path
 from cosmetics_API import views
 
 urlpatterns = [
-    path('items', views.itemList),
-    path('items/<int:item_id>', views.item_one),
-    path('items/recom/<int:user_id>', views.itemListRecom),
-    path('items/recom/<int:user_id>/<str:category_large>', views.itemListRecomCategoryLarge),
-    path('items/recom/<int:user_id>/<str:category_large>/<str:category_middle>', views.itemListRecomCategoryMiddle),
-    path('items/sort/score/<int:user_id>', views.itemSortScore),
-    path('items/sort/score/<int:user_id>/<str:category_large>', views.itemSortScore),
-    path('items/sort/score/<int:user_id>/<str:category_large>/<str:category_middle>', views.itemSortScore),
-    path('items/sort/reviewCnt/<int:user_id>', views.itemSortReviewCnt),
-    path('items/sort/reviewCnt/<int:user_id>/<str:category_large>', views.itemSortReviewCnt),
-    path('items/sort/reviewCnt/<int:user_id>/<str:category_large>/<str:category_middle>', views.itemSortReviewCnt),
-    path('items/sort/price/<int:user_id>/<int:sort_type>', views.itemSortPrice),
-    path('items/sort/price/<int:user_id>/<str:category_large>/<int:sort_type>', views.itemSortPrice),
-    path('items/sort/price/<int:user_id>/<str:category_large>/<str:category_middle>/<int:sort_type>', views.itemSortPrice),
+    path('', views.itemList),
+    path('<int:item_id>', views.item_one),
+    path('recom/<int:user_id>', views.itemListRecom),
+    path('recom/<int:user_id>/<str:category_large>', views.itemListRecomCategoryLarge),
+    path('recom/<int:user_id>/<str:category_large>/<str:category_middle>', views.itemListRecomCategoryMiddle),
+    path('sort/score/<int:user_id>', views.itemSortScore),
+    path('sort/score/<int:user_id>/<str:category_large>', views.itemSortScore),
+    path('sort/score/<int:user_id>/<str:category_large>/<str:category_middle>', views.itemSortScore),
+    path('sort/reviewCnt/<int:user_id>', views.itemSortReviewCnt),
+    path('sort/reviewCnt/<int:user_id>/<str:category_large>', views.itemSortReviewCnt),
+    path('sort/reviewCnt/<int:user_id>/<str:category_large>/<str:category_middle>', views.itemSortReviewCnt),
+    path('sort/price/<int:user_id>/<int:sort_type>', views.itemSortPrice),
+    path('sort/price/<int:user_id>/<str:category_large>/<int:sort_type>', views.itemSortPrice),
+    path('sort/price/<int:user_id>/<str:category_large>/<str:category_middle>/<int:sort_type>', views.itemSortPrice),
     
 ]
---
+++ backend/PythonSera/personal_API/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class PersonalApiConfig(AppConfig):
+    name = 'personal_API'

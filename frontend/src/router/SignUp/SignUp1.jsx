7dccf0ca72c9d873503173ce4a70e44dbc93a3d4
dovvn <jdaun.dev@gmail.com>
2021-04-07 07:48:48 +0900

    FEBE_0407: 닉네임,아이디 중복확인 백수정 & swal적용
    Former-commit-id: 0550ef272acade37e43e4491d8f8aacb18ead16c

--- frontend/src/router/SignUp/SignUp1.jsx
+++ frontend/src/router/SignUp/SignUp1.jsx
@@ -3,6 +3,7 @@ import styles from "./SignUp1.module.css";
 import Grid from "@material-ui/core/Grid";
 import { useHistory } from "react-router-dom";
 import http from "../../http-common.js";
+import Swal from "sweetalert2";
 
 const SignUp1 = () => {
   const history = useHistory();
@@ -21,95 +22,178 @@ const SignUp1 = () => {
   const [ableNickname, setAbleNickname] = useState(false);
   const [ablePassword, setAblePassword] = useState(false);
 
-
   const onChangeUserLoginId = (e) => {
     setAbleLoginId(false);
     setUserLoginId(e.target.value);
-    if (((5<=e.target.value.length) && (e.target.value.length <= 12)) && !(RegExp.test(e.target.value) || upperCase.test(e.target.value) || regKorean.test(e.target.value))) setIdButtonColor("#FD6C1D");
+    if (
+      5 <= e.target.value.length &&
+      e.target.value.length <= 12 &&
+      !(
+        RegExp.test(e.target.value) ||
+        upperCase.test(e.target.value) ||
+        regKorean.test(e.target.value)
+      )
+    )
+      setIdButtonColor("#FD6C1D");
     else setIdButtonColor("#666");
   };
 
   const onChangeUserNickname = (e) => {
     setAbleNickname(false);
     setUserNickname(e.target.value);
-    if ((4<=e.target.value.length && e.target.value.length<=6) && !RegExp.test(e.target.value)) setNameButtonColor("#FD6C1D");
+    console.log(e.target.value);
+    if ((4 <= e.target.value.length && e.target.value.length <= 6) && !RegExp.test(e.target.value)) setNameButtonColor("#FD6C1D");
     else setNameButtonColor("#666");
   };
 
   const onChangeUserPassword = (e) => {
     setAblePassword(false);
     setUserPassword(e.target.value);
-    if(userPassword2 === e.target.value && (userPassword2 !== '' && e.target.value!=='')) setAblePassword(true);
+    if (
+      userPassword2 === e.target.value &&
+      userPassword2 !== "" &&
+      e.target.value !== "" && e.target.value.length>=6 && e.target.value.length<=15
+    )
+      setAblePassword(true);
   };
 
   const onChangeUserPassword2 = (e) => {
     setAblePassword(false);
     setUserPassword2(e.target.value);
-    if(userPassword === e.target.value && (userPassword !== '' && e.target.value !== '')) setAblePassword(true);
-  }
+    if (
+      userPassword === e.target.value &&
+      userPassword !== "" &&
+      e.target.value !== "" && e.target.value.length>=6 && e.target.value.length<=15
+    )
+      setAblePassword(true);
+  };
 
   useEffect(() => {
-      console.log(ableLoginId+", "+ableNickname+", "+ablePassword);
-      if(ableLoginId && ableNickname && ablePassword) setNextColor("#FD6C1D");
-      else setNextColor("#999");
+    console.log(ableLoginId + ", " + ableNickname + ", " + ablePassword);
+    if (ableLoginId && ableNickname && ablePassword) setNextColor("#FD6C1D");
+    else setNextColor("#999");
   }, [ableLoginId, ableNickname, ablePassword]);
 
-  
   var upperCase = /[A-Z]/; //대문자
-  var regKorean =  /^[ㄱ-ㅎ가-힣]+$/; //한글 정규식
-  var RegExp =  /[~!@#$%^&*()_+|<>?:{}]/;//특수문자 정규식
+  var regKorean = /^[ㄱ-ㅎ가-힣]+$/; //한글 정규식
+  var RegExp = /[~!@#$%^&*()_+|<>?:{}]/; //특수문자 정규식
 
   //아이디 중복확인
   const checkUserLoginId = () => {
     if (idButtonColor === "#FD6C1D") {
-     http
-     .get("v1/users/duplicate/" + userLoginId)
-     .then((res) => {
-       if (res.data.status === "success") { //사용가능
-         alert("사용가능한 아이디입니다");  
-         setAbleLoginId(true);
-       }else alert("이미 중복된 아이디입니다.");
-     })
-     .catch((err) => {
-       console.error(err);
-     });
-    }else alert("아이디 형식이 올바르지 않습니다");
+      http
+        .get("v1/users/duplicate/" + userLoginId)
+        .then((res) => {
+          console.log(res.data);
+          if (res.data.data) {
+            Swal.fire({
+              icon: "error",
+              text: "이미 중복된 아이디입니다.",
+              showConfirmButton: false,
+              timer: 2000,
+            });
+          } else {
+            Swal.fire({
+              icon: "success",
+              text: "사용 가능한 아이디입니다.",
+              showConfirmButton: false,
+              timer: 2000,
+            });
+            setAbleLoginId(true);
+          }
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+    } else {
+      Swal.fire({
+        icon: "error",
+        text: "아이디 형식이 올바르지 않습니다.",
+        showConfirmButton: false,
+        timer: 2000,
+      });
+    }
   };
 
   //닉네임 중복확인
   const checkUserNickname = () => {
     //닉네임 양식 체크
-    if (NameButtonColor === "#FD6C1D"){
-        http
+    if (NameButtonColor === "#FD6C1D") {  
+      http
         .get("v1/users/duplicate/nickname/" + userNickname)
         .then((res) => {
-            if (res.data.status === "success") { //사용가능
-                alert("사용가능한 닉네임입니다.");
-                setAbleNickname(true);
-            }else alert("이미 중복된 닉네임입니다.");
+          if (res.data.data) {
+            Swal.fire({
+              icon: "error",
+              text: "이미 중복된 닉네임입니다.",
+              showConfirmButton: false,
+              timer: 2000,
+            });
+          } else {
+            Swal.fire({
+              icon: "success",
+              text: "사용 가능한 닉네임입니다.",
+              showConfirmButton: false,
+              timer: 2000,
+            });
+            setAbleNickname(true);
+          }
         })
         .catch((err) => {
-            console.error(err);
+          console.error(err);
         });
-    }else alert("닉네임 형식이 올바르지 않습니다");
+    } else{
+      Swal.fire({ 
+        icon: "error",
+        text: "닉네임 형식이 올바르지 않습니다.",
+        showConfirmButton: false,
+        timer: 2000,
+      });
+    }
   };
-  
 
   //다음 페이지 이동
   const onNext = (e) => {
-      if(nextColor === "#FD6C1D"){
-            history.push({
-                pathname:'/signup2',
-                state:{
-                    userLoginId, userNickname, userPassword
-                }
-            });
-       }else{
-         if(!ableLoginId) alert("아이디 중복확인을 해주세요");
-         else if(!ableNickname) alert("닉네임 중복확인을 해주세요");
-         else if(!ablePassword) alert("비밀번호가 일치하지 않습니다");
-         else alert("모든 입력폼을 작성해주세요");
-       }
+    if (nextColor === "#FD6C1D") {
+      history.push({
+        pathname: "/signup2",
+        state: {
+          userLoginId,
+          userNickname,
+          userPassword,
+        },
+      });
+    } else {
+      if (!ableLoginId) {
+        Swal.fire({
+          icon: "error",
+          text: "아이디 중복확인을 해주세요",
+          showConfirmButton: false,
+          timer: 2000,
+        });
+      } else if (!ableNickname) {
+        Swal.fire({
+          icon: "error",
+          text: "닉네임 중복확인을 해주세요",
+          showConfirmButton: false,
+          timer: 2000,
+        });
+      } else if (!ablePassword) {
+        Swal.fire({
+          icon: "error",
+          text: "비밀번호가 올바르지 않습니다",
+          showConfirmButton: false,
+          timer: 2000,
+        });
+      } else {
+        Swal.fire({
+          icon: "error",
+          text: "모든 입력폼을 작성해주세요",
+          showConfirmButton: false,
+          timer: 2000,
+        });
+      }
+    }
   };
 
   return (
@@ -133,7 +217,7 @@ const SignUp1 = () => {
                   placeholder="5~12자의 소문자,숫자,-,_만 사용가능합니다"
                   onChange={onChangeUserLoginId}
                   value={userLoginId}
-                  maxLength="12"
+                  maxlength="12"
                 />
                 <input
                   className={styles.input_btn}
@@ -154,7 +238,6 @@ const SignUp1 = () => {
                   placeholder="4~6자 이하의 한글,숫자 조합만 가능합니다"
                   onChange={onChangeUserNickname}
                   value={userNickname}
-                  maxlenth="6"
                 />
                 <input
                   className={styles.input_btn}
@@ -194,7 +277,11 @@ const SignUp1 = () => {
               </li>
             </ul>
           </form>
-          <div className={styles.nextBtn} onClick={onNext} style={{color: nextColor}}>
+          <div
+            className={styles.nextBtn}
+            onClick={onNext}
+            style={{ color: nextColor }}
+          >
             next &nbsp;&nbsp; >
           </div>
         </div>

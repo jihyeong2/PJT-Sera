149b6480fb48a4bc4ee5c0f32363c489c73095b9
dovvn <jdaun.dev@gmail.com>
2021-04-07 06:45:11 +0900

    FEBE_0407: 로그인,가입,비번 조건 업데이트/아이디기억하기 완료
    Former-commit-id: c557b630ead768bd6b4241281f7a1c7049982d4e [formerly 6a173ba7bc903766128575401ccd7255c53ecf40]
    Former-commit-id: ac43c961976d8ab54367522c125040b4e6413156

--- frontend/src/components/CosmeticDetail/review/review_chart.jsx
+++ frontend/src/components/CosmeticDetail/review/review_chart.jsx
@@ -1,44 +1,47 @@
-import React from "react";
+import { FreeBreakfastRounded } from "@material-ui/icons";
+import React, {useState, useEffect} from "react";
 import styles from "./review_chart.module.css";
 
-const ReviewChart = () => {
+const ReviewChart = ({}) => {
+
+
   return (
     <div className={styles.graph_area}>
       <ul className={styles.graph_list}>
         <li>
-          <span className={styles.per}>11%</span>
+          <span className={styles.per}>1%</span>
           <div className={styles.graph}>
             <span></span>
           </div>
           <span className={styles.txt}>5점</span>
         </li>
         <li>
-          <span className={styles.per}>22%</span>
+          <span className={styles.per}>1%</span>
           <div className={styles.graph}>
             <span></span>
           </div>
           <span className={styles.txt}>4점</span>
         </li>
         <li>
-          <span className={styles.per}>33%</span>
+          <span className={styles.per}>1%</span>
           <div className={styles.graph}>
             <span></span>
           </div>
-          <span className={styles.txt}>5점</span>
+          <span className={styles.txt}>3점</span>
         </li>
         <li>
-          <span className={styles.per}>44%</span>
+          <span className={styles.per}>1%</span>
           <div className={styles.graph}>
             <span></span>
           </div>
-          <span className={styles.txt}>5점</span>
+          <span className={styles.txt}>2점</span>
         </li>
         <li>
-          <span className={styles.per}>55%</span>
+          <span className={styles.per}>1%</span>
           <div className={styles.graph}>
             <span></span>
           </div>
-          <span className={styles.txt}>5점</span>
+          <span className={styles.txt}>1점</span>
         </li>
       </ul>
     </div>

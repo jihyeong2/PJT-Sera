9cfb38c5f9285c5e8e3dd3e83bf661e45d01593f
Ji-hyeong <qmffn38@gmail.com>
2021-04-08 15:43:51 +0900

    exec 폴더 생성 & api baseURL 수정
    Former-commit-id: de081b63c08cd70966e2151593f5d70379805534

---
+++ exec/backend/SpringSera/src/main/java/com/ssafy/sera/Domain/review/Review.java
@@ -0,0 +1,77 @@
+package com.ssafy.sera.Domain.review;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.ssafy.sera.Controller.Request.ReviewRequest;
+import com.ssafy.sera.Domain.Item.Item;
+import com.ssafy.sera.Domain.User.User;
+import lombok.Getter;
+import lombok.Setter;
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
+@Entity
+@Getter @Setter
+
+public class Review implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long reviewId;
+
+    @Column(columnDefinition = "TEXT")
+    private String reviewImg;
+
+    @JsonBackReference
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="item_id")
+    private Item item; //게시글-상품과의 관계: N:1
+
+    @JsonBackReference
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="user_id")
+    private User user; //게시글-회원과의 관계: N:1
+
+    @Column(nullable = false)
+    private Date writeDate;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String reviewGoodContent;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String reviewBadContent;
+
+    @Column(nullable = false)
+    private int reviewScore;
+
+    @Column(nullable = false)
+    private Long helpCnt;
+
+    @Transient
+    private int helpMark; //도움 여부(1:도움, 0:도움x)
+
+    public static Review createReview(ReviewRequest reviewRequest){
+        Review reviewInput = new Review();
+        reviewInput.setReviewImg(reviewRequest.getReviewImg());
+        reviewInput.setWriteDate(reviewRequest.getWriteDate());
+        reviewInput.setReviewGoodContent(reviewRequest.getReviewGoodContent());
+        reviewInput.setReviewBadContent(reviewRequest.getReviewBadContent());
+        reviewInput.setReviewScore(reviewRequest.getReviewScore());
+        reviewInput.setHelpCnt(reviewRequest.getHelpCnt());
+        return reviewInput;
+    }
+
+    @PrePersist
+    private void onCreate(){
+        this.writeDate = new Date();
+    }
+
+
+    public long pushCnt(){
+        this.helpCnt += 1;
+        return this.helpCnt;
+    }
+    public long pullCnt(){
+        this.helpCnt -= 1;
+        return this.helpCnt;
+    }
+}

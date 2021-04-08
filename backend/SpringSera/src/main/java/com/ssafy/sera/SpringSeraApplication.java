385fc2c62b307f04d02365c9f6fb4d78e59afef9
dovvn <jdaun.dev@gmail.com>
2021-04-08 14:05:55 +0900

    BE_0205: 스프링 빌드 에러 해결중
    Former-commit-id: 6c2954974ce0737a6a363b7f6f303290e00d9966

--- backend/SpringSera/src/main/java/com/ssafy/sera/SpringSeraApplication.java
+++ backend/SpringSera/src/main/java/com/ssafy/sera/SpringSeraApplication.java
@@ -2,9 +2,10 @@ package com.ssafy.sera;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.aws.autoconfigure.context.ContextRegionProviderAutoConfiguration;
 
-@SpringBootApplication
-public class SpringSeraApplication {
+@SpringBootApplication(exclude =ContextRegionProviderAutoConfiguration.class)
+public class SpringSeraApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringSeraApplication.class, args);

c0a921116f12798d63c55c041fdc61a11dd1e054
dovvn <jdaun.dev@gmail.com>
2021-04-08 14:26:51 +0900

    BE_0408: 스프링 에러 해결
    Former-commit-id: 1da8aa9821cbce93d9f51b160673af58ef09594b

--- backend/SpringSera/src/main/java/com/ssafy/sera/SpringSeraApplication.java
+++ backend/SpringSera/src/main/java/com/ssafy/sera/SpringSeraApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.aws.autoconfigure.context.ContextRegionProviderAutoConfiguration;
 
-@SpringBootApplication(exclude =ContextRegionProviderAutoConfiguration.class)
+@SpringBootApplication
 public class SpringSeraApplication{
 
 	public static void main(String[] args) {

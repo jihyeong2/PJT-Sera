97ac6fae8ab935b6a4087beeb3db7bb63bc627a7
dovvn <jdaun.dev@gmail.com>
2021-04-08 11:47:46 +0900

    FE_0408: 리뷰차트 완료
    Former-commit-id: 2897c8bf0144190eccd5c0958991e6f052a704ce

--- frontend/src/components/CosmeticDetail/review/review_chart.jsx
+++ frontend/src/components/CosmeticDetail/review/review_chart.jsx
@@ -8,37 +8,37 @@ const ReviewChart = ({grade}) => {
     <div className={styles.graph_area}>
       <ul className={styles.graph_list}>
         <li>
-          <span className={styles.per}>{Math.ceil((grade.star_5 / grade.star_cnt)*100)}%</span>
+          <span className={styles.per}>{grade.star_5 != 0 ? Math.ceil((grade.star_5 / grade.star_cnt)*100) : 0}%</span>
           <div className={styles.graph}>
-            <span style={{height: Math.ceil((grade.star_5 / grade.star_cnt)*100)}}></span>
+            <span style={{height: grade.star_5 != 0 ? Math.ceil((grade.star_5 / grade.star_cnt)*100) : 0}}></span>
           </div>
           <span className={styles.txt}>5점</span>
         </li>
         <li>
-          <span className={styles.per}>{Math.ceil((grade.star_4 / grade.star_cnt)*100)}%</span>
+          <span className={styles.per}>{grade.star_4 != 0 ? Math.ceil((grade.star_4 / grade.star_cnt)*100) : 0}%</span>
           <div className={styles.graph}>
-            <span style={{height: Math.ceil((grade.star_4 / grade.star_cnt)*100)}}></span>
+            <span style={{height: grade.star_4 != 0 ? Math.ceil((grade.star_4 / grade.star_cnt)*100) : 0}}></span>
           </div>
           <span className={styles.txt}>4점</span>
         </li>
         <li>
-          <span className={styles.per}>{Math.ceil((grade.star_3 / grade.star_cnt)*100)}%</span>
+          <span className={styles.per}>{grade.star_3 != 0 ? Math.ceil((grade.star_3 / grade.star_cnt)*100) : 0}%</span>
           <div className={styles.graph}>
-            <span style={{height: Math.ceil((grade.star_3 / grade.star_cnt)*100)}}></span>
+            <span style={{height: grade.star_3 != 0 ? Math.ceil((grade.star_3 / grade.star_cnt)*100) : 0}}></span>
           </div>
           <span className={styles.txt}>3점</span>
         </li>
         <li>
-          <span className={styles.per}>{Math.ceil((grade.star_2 / grade.star_cnt)*100)}%</span>
+          <span className={styles.per}>{grade.star_2 != 0 ? Math.ceil((grade.star_2 / grade.star_cnt)*100) : 0}%</span>
           <div className={styles.graph}>
-            <span style={{height: Math.ceil((grade.star_2 / grade.star_cnt)*100)}}></span>
+            <span style={{height: grade.star_2 != 0 ? Math.ceil((grade.star_2 / grade.star_cnt)*100) : 0}}></span>
           </div>
           <span className={styles.txt}>2점</span>
         </li>
         <li>
-          <span className={styles.per}>{Math.ceil((grade.star_1 / grade.star_cnt)*100)}%</span>
+          <span className={styles.per}>{grade.star_1 != 0 ? Math.ceil((grade.star_1 / grade.star_cnt)*100) : 0}%</span>
           <div className={styles.graph}>
-            <span style={{height: Math.ceil((grade.star_1 / grade.star_cnt)*100)}}></span>
+           <span style={{height: grade.star_1 != 0 ? Math.ceil((grade.star_1 / grade.star_cnt)*100) : 0}}></span>
           </div>
           <span className={styles.txt}>1점</span>
         </li>

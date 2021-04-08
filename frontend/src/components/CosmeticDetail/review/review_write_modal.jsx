0b9da657e281bef640afd62a556ff05940b3702f
유진이 <wlsdk400@naver.com>
2021-04-08 14:19:02 +0900

    FE_리뷰수정
    Former-commit-id: 9b2777c44b3b8c694c103cb960b4315e50cab7c0

--- frontend/src/components/CosmeticDetail/review/review_write_modal.jsx
+++ frontend/src/components/CosmeticDetail/review/review_write_modal.jsx
@@ -21,7 +21,7 @@ const ReviewWrite = ({product, user, onCreateReview}) => {
         if (fileExt !== '.jpg' && fileExt !== '.png' && fileExt !== '.gif') {
             Swal.fire({
                 icon: 'error',
-                text: "png, gif, jpg형식의 사진만 업로드 가능합니다",
+                text: "png, jpg형식의 사진만 업로드 가능합니다",
                 showConfirmButton: false,
                 timer: 1000
               });
@@ -153,10 +153,10 @@ const ReviewWrite = ({product, user, onCreateReview}) => {
                     <div className={styles.fileBox}>
                         <span class={styles.fileName}>{fileName}</span>
                         <label>
-                            <input type="file" name="picture" className={styles.fileInput} onChange={changeFileName} />
+                            <input type="file" name="picture" accept="image/png, image/jpeg" className={styles.fileInput} onChange={changeFileName} />
                         </label>
                     </div>
-                    <span>* 사진은 1MB이하의 PNG, GIF, JPG 파일만 등록 가능합니다.</span>
+                    <span>* 사진은 1MB이하의 PNG, JPG 파일만 등록 가능합니다.</span>
                 </div>
                 <div className={styles.buttons}>
                     <div className={styles.all_btns}>
